9c4609dc252785bb617f2c478e6a2f4639a7b072
AFG222 <ahmetfurkangazioglu@gmail.com>
2021-02-18 04:04:28 +0300

    18.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,15 +9,24 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+           // ProductTest();
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var products in productManager.GetAllByUnitPrice(50,100))
+            foreach (var products in productManager.GetAllByUnitPrice(50, 100))
             {
                 Console.WriteLine(products.ProductName);
-
-
-
             }
-        }      
+        }
     } 
 }
--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -15,5 +15,6 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Product> products { get; set; }
         public DbSet<Category> categories { get; set; }
         public DbSet<Customer> customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
