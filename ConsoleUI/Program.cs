8d5b89b397c4996daf29432a03c206d64afbef1e
AFG222 <ahmetfurkangazioglu@gmail.com>
2021-02-19 01:49:46 +0300

    19.02.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,23 +9,28 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-           // ProductTest();
+            ProductTest();
 
+          //  CategoryText();
+
+        }
+
+        private static void CategoryText()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
             }
-
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var products in productManager.GetAllByUnitPrice(50, 100))
+            foreach (var products in productManager.GetProductsDetail())
             {
-                Console.WriteLine(products.ProductName);
+                Console.WriteLine(products.ProductName+" / "+products.CategoryName);
             }
         }
     } 
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,8 +11,18 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal :EfEntityRepositoryBase<Product,NorthwindContext> ,IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-       
+        public List<ProductDetailDto> GetProductsDetail()
+        {
+            using (NorthwindContext context= new NorthwindContext())
+            {
+                var result = from p in context.products
+                             join c in context.categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto { ProductId = p.ProductId, ProductName = p.ProductName, UnitsInStock = p.UnitsInStock, CategoryName = c.CategoryName };
+               return result.ToList();
+            }
+        }
     }
 }
