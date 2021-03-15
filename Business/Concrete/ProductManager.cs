d139d4c8a836a2b9ac2767930aa9744558fe905e
AFG222 <ahmetfurkangazioglu@gmail.com>
2021-03-15 03:38:41 +0300

    Update

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -50,7 +50,7 @@ namespace Business.Concrete
         [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 5)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTimer);
             }
